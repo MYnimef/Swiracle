7b4bf1ab566b72934b394511bf52893bd4073fb0
Ivan <ivan.markov.2013@gmail.com>
2021-04-01 14:36:11 +0300

    0.0.7

---
+++ app/src/main/java/com/mynimef/swiracle/CreateActivity.java
@@ -0,0 +1,6 @@
+package com.mynimef.swiracle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/mynimef/swiracle/MainActivity.java
+++ app/src/main/java/com/mynimef/swiracle/MainActivity.java
@@ -1,13 +1,19 @@
 package com.mynimef.swiracle;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mynimef.swiracle.ui.home.HomeFragment;
+import com.mynimef.swiracle.ui.notifications.NotificationsFragment;
+import com.mynimef.swiracle.ui.profile.ProfileFragment;
+import com.mynimef.swiracle.ui.search.SearchFragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +22,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        //AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                //R.id.navigation_home, R.id.navigation_search, R.id.navigation_create, R.id.navigation_notifications, R.id.navigation_profile)
-                //.build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navigation_create) {
+                    startActivity(new Intent(MainActivity.this, CreateActivity.class));
+                } else {
+                    FragmentManager fm = getSupportFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    if (itemId == R.id.navigation_home) {
+                        ft.add(R.id.nav_host_fragment, new HomeFragment());
+                    } else if (itemId == R.id.navigation_search) {
+                        ft.add(R.id.nav_host_fragment, new SearchFragment());
+                    }  else if (itemId == R.id.navigation_notifications) {
+                        ft.add(R.id.nav_host_fragment, new NotificationsFragment());
+                    } else if (itemId == R.id.navigation_profile) {
+                        ft.add(R.id.nav_host_fragment, new ProfileFragment());
+                    }
+                    ft.commit();
+                }
+                return true;
+            }
+        });
     }
 }
