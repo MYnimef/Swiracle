845b4c26ac6d1659a4f1826a84cda196613b593e
Ivan <ivan.markov.2013@gmail.com>
2021-04-01 14:56:29 +0300

    0.0.8

--- app/src/main/java/com/mynimef/swiracle/ui/home/HomeFragment.java
+++ app/src/main/java/com/mynimef/swiracle/ui/home/HomeFragment.java
@@ -8,13 +8,10 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.mynimef.swiracle.R;
 
 public class HomeFragment extends Fragment {
-
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
