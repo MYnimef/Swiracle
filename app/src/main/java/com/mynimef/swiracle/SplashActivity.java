3805fd488c2e544e1a697248bbb59f37a596b4ec
Ivan <ivan.markov.2013@gmail.com>
2021-07-29 11:57:43 +0300

    0.3.11

--- app/src/main/java/com/mynimef/swiracle/SplashActivity.java
+++ app/src/main/java/com/mynimef/swiracle/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.mynimef.swiracle;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -7,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
+@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 public final class SplashActivity extends AppCompatActivity {
 
